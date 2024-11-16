7281f2231d60e0541922475871263adb176bc452
CatHat <liase810@gmail.com>
2024-11-16 13:07:05 +0100

    init

---
+++ public/profiles.ts
@@ -0,0 +1,15 @@
+type Profile = {
+    name: string
+    path: `/public/${string}/imgFiles.ts`
+}
+
+export default [
+    {
+        name: 'Valorant',
+        path: '/public/valorant/imgFiles.ts',
+    },
+    {
+        name: 'Overwatch',
+        path: '/public/overwatch/imgFiles.ts',
+    },
+] satisfies Profile[]
---
+++ public/profiles/valorant/imgFiles.ts
@@ -0,0 +1,54 @@
+/// <reference types="../imgFiles.d.ts" />
+import Astra from './images/Astra.png'
+import Breach from './images/Breach.png'
+import Brimstone from './images/Brimstone.png'
+import Chamber from './images/Chamber.png'
+import Clove from './images/Clove.png'
+import Cypher from './images/Cypher.png'
+import Deadlock from './images/Deadlock.png'
+import Fade from './images/Fade.png'
+import Gekko from './images/Gekko.png'
+import Harbor from './images/Harbor.png'
+import Iso from './images/Iso.png'
+import Jett from './images/Jett.png'
+import KAYO from './images/KAYO.png'
+import Killjoy from './images/Killjoy.png'
+import Neon from './images/Neon.png'
+import Omen from './images/Omen.png'
+import Phoenix from './images/Phoenix.png'
+import Raze from './images/Raze.png'
+import Reyna from './images/Reyna.png'
+import Sage from './images/Sage.png'
+import Skye from './images/Skye.png'
+import Sova from './images/Sova.png'
+import Viper from './images/Viper.png'
+import Vyse from './images/Vyse.png'
+import Yoru from './images/Yoru.png'
+
+export default [
+    { name: 'Astra', img: Astra },
+    { name: 'Breach', img: Breach },
+    { name: 'Brimstone', img: Brimstone },
+    { name: 'Chamber', img: Chamber },
+    { name: 'Clove', img: Clove },
+    { name: 'Cypher', img: Cypher },
+    { name: 'Deadlock', img: Deadlock },
+    { name: 'Fade', img: Fade },
+    { name: 'Gekko', img: Gekko },
+    { name: 'Harbor', img: Harbor },
+    { name: 'Iso', img: Iso },
+    { name: 'Jett', img: Jett },
+    { name: 'KAY/O', img: KAYO },
+    { name: 'Killjoy', img: Killjoy },
+    { name: 'Neon', img: Neon },
+    { name: 'Omen', img: Omen },
+    { name: 'Phoenix', img: Phoenix },
+    { name: 'Raze', img: Raze },
+    { name: 'Reyna', img: Reyna },
+    { name: 'Sage', img: Sage },
+    { name: 'Skye', img: Skye },
+    { name: 'Sova', img: Sova },
+    { name: 'Viper', img: Viper },
+    { name: 'Vyse', img: Vyse },
+    { name: 'Yoru', img: Yoru },
+]
